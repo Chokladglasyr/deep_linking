2b4d34e9b2bdd04f5be6615badd10535092ae751
Chokladglasyr <lim_ida@live.com>
2025-08-25 14:06:29 +0200

    feat: added static page with register form

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,17 @@
 import express, { Request, Response} from 'express'
 import dotenv from 'dotenv';
 import {createUser} from '../controllers/userController'
+import path from 'path';
 
 dotenv.config();
 
 const app = express()
 app.use(express.json())
 
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
 app.get('/', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
     res.send("Hej")
 })
 
