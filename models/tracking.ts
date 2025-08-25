0361d5046a194aa28e57db3231f632bdb2a80c90
FredricLaestander <fredriclaestander@gmail.com>
2025-08-25 23:52:10 +0200

    create tracking model and rename folder to models

---
+++ models/tracking.ts
@@ -0,0 +1,12 @@
+import { model, Schema } from "mongoose";
+
+const trackingSchema = new Schema({
+  ip: { type: String, required: true },
+  influencer: { type: String, required: true },
+  source: { type: String },
+});
+
+export const Tracking = model<Schema<typeof trackingSchema>>(
+  "Tracking",
+  trackingSchema
+);
---
+++ models/userInterface.ts
@@ -0,0 +1,14 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
+}
+const userSchema: Schema = new Schema({
+  _id: { type: String },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+});
+export const User = mongoose.model<IUser>("User", userSchema);
